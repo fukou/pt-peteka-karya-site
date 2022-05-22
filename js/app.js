80277184da823c9106707b3731073e75eee49979
faiz.j <faiz.j@softwareseni.com>
2022-05-22 21:25:42 +0700

    feat: add the new teaser site & other adjustments

--- js/app.js
+++ js/app.js
@@ -14,32 +14,41 @@ var app = {
         app.aosAnimation();
         app.loadTabs();
         app.dummyMaps();
+        app.countDownTimer();
     },
     searchToggle: () => {
-       const btnSearch = document.querySelector('button[data-type-button="search"]');
+       let btnSearch = document.querySelector('button[data-type-button="search"]');
        const wrapperSearch = document.querySelector('.header__search');
+       
+       const isButtonExist = btnSearch;
+       if(typeof(isButtonExist) != 'undefined' && isButtonExist != null) {
 
-       btnSearch.addEventListener("click", function() {
-        wrapperSearch.classList.toggle("is-shown");
-        if(wrapperSearch.classList.contains('is-shown')) {
-            this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
-        } else {
-            this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>`;
-        }
-       });
+        btnSearch.addEventListener("click", function() {
+         wrapperSearch.classList.toggle("is-shown");
+         if(wrapperSearch.classList.contains('is-shown')) {
+             this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
+         } else {
+             this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>`;
+         }
+        });
+       }
     },
     mobileToggle: () => {
         const btnHamburger = document.querySelector('button[data-type-button="hamburger"]');
         const wrapperMenu = document.querySelector('.header__links');
 
-        btnHamburger.addEventListener("click", function() {
-            wrapperMenu.classList.toggle("is-shown");
-            if(wrapperMenu.classList.contains('is-shown')) {
-                this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
-            } else {
-                this.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>`;
-            }
-           });
+        const isButtonExist = btnHamburger;
+        if(typeof(isButtonExist) != 'undefined' && isButtonExist != null) {
+
+            btnHamburger.addEventListener("click", function() {
+                wrapperMenu.classList.toggle("is-shown");
+                if(wrapperMenu.classList.contains('is-shown')) {
+                    this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
+                } else {
+                    this.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>`;
+                }
+            });
+        }
     },
     mobileSubmenuToggle: () => {
         const submenu = document.querySelectorAll(".header ul > li.header__links__menu--item--has-submenu > a");
@@ -115,6 +124,39 @@ var app = {
             }).addTo(map);
         }
     },
+    countDownTimer: () => {
+        const daysEl = document.getElementById("days");
+        const hoursEl = document.getElementById("hours");
+        const minsEl = document.getElementById("mins");
+        const secondsEl = document.getElementById("seconds");
+
+        const newYears = "1 July 2022";
+
+        function countdown() {
+            const newYearsDate = new Date(newYears);
+            const currentDate = new Date();
+
+            const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+            const days = Math.floor(totalSeconds / 3600 / 24);
+            const hours = Math.floor(totalSeconds / 3600) % 24;
+            const mins = Math.floor(totalSeconds / 60) % 60;
+            const seconds = Math.floor(totalSeconds) % 60;
+
+            daysEl.innerHTML = days;
+            hoursEl.innerHTML = formatTime(hours);
+            minsEl.innerHTML = formatTime(mins);
+            secondsEl.innerHTML = formatTime(seconds);
+        }
+
+        function formatTime(time) {
+            return time < 10 ? `0${time}` : time;
+        }
+
+        // initial call
+        countdown();
+        setInterval(countdown, 1000);
+    }
 };
   
 document.addEventListener("DOMContentLoaded", () => {
