d44a9959cd59797ce92c21493cf542816a137a34
faiz.j <faiz.j@softwareseni.com>
2022-06-14 20:10:30 +0700

    fix: adjust the search button toggle issue

--- js/app.js
+++ js/app.js
@@ -38,20 +38,24 @@ var app = {
        });
     },
     searchToggle: () => {
-       let btnSearch = document.querySelector('button[data-type-button="search"]');
+       let btnSearch = document.querySelectorAll('button[data-type-button="search"]');
        const wrapperSearch = document.querySelector('.header__search');
        
        const isButtonExist = btnSearch;
        if(typeof(isButtonExist) != 'undefined' && isButtonExist != null) {
 
-        btnSearch.addEventListener("click", function() {
-         wrapperSearch.classList.toggle("is-shown");
-         if(wrapperSearch.classList.contains('is-shown')) {
-             this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
-         } else {
-             this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>`;
-         }
-        });
+        for (const b of btnSearch) {
+            b.addEventListener("click", function() {
+                wrapperSearch.classList.toggle("is-shown");
+                if(wrapperSearch.classList.contains('is-shown')) {
+                    this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`;
+                } else {
+                    this.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>`;
+                }
+            });
+        }
+
+        
        }
     },
     mobileToggle: () => {
